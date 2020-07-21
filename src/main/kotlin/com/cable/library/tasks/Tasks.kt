6dd1e55d3e0fd52e8ca53b3e85a32ed18ea249c6
Paul Preston <paul_l_preston@hotmail.com>
2020-07-21 21:53:25 +1000

    Turns out Task works by ticks, duh

--- src/main/kotlin/com/cable/library/tasks/Tasks.kt
+++ src/main/kotlin/com/cable/library/tasks/Tasks.kt
@@ -4,6 +4,6 @@ package com.cable.library.tasks
  * Simple shortcut to the Task builder for simple, delayed, synchronous actions.
  */
 public fun after(seconds: Number, action: () -> Unit): Any = Task.builder()
-        .delay((seconds.toDouble() * 1000L).toLong())
+        .delay((seconds.toDouble()/20.0).toLong())
         .execute(action)
         .build()
