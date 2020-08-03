b2acbb2336f6a7b1617dd46022ab4b8603b51ca3
CraftSteamG <harrisonnathan2002@gmail.com>
2020-08-03 13:34:24 -0400

    Added the addition of a teleport utility, which works on cross-dim travel.

---
+++ src/main/java/com/cable/library/teleport/TeleportUtils.java
@@ -0,0 +1,36 @@
+package com.cable.library.teleport;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.common.util.ITeleporter;
+
+public class TeleportUtils {
+
+    public static void teleport(EntityPlayerMP player, World world, BlockPos pos) {
+        int to = world.provider.getDimension();
+        int from = player.getServerWorld().provider.getDimension();
+
+        if (to == from) {
+            player.moveToBlockPosAndAngles(pos, player.rotationYaw, player.rotationPitch);
+        } else {
+            player.changeDimension(to, new SimpleTeleporter(pos));
+        }
+
+    }
+
+    private static class SimpleTeleporter implements ITeleporter {
+
+        private BlockPos pos;
+
+        private SimpleTeleporter(BlockPos pos) {
+            this.pos = pos;
+        }
+
+        @Override
+        public void placeEntity(World world, Entity entity, float yaw) {
+            entity.moveToBlockPosAndAngles(pos, yaw, entity.rotationPitch);
+        }
+    }
+}
