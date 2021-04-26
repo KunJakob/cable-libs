ea8971b2f996882fb337efafb5bf4d64ea5881a9
Paul <paul_l_preston@hotmail.com>
2021-04-26 18:46:18 +1000

    Added more precision potential to TeleportUtils

--- src/main/java/com/cable/library/teleport/TeleportUtils.java
+++ src/main/java/com/cable/library/teleport/TeleportUtils.java
@@ -3,6 +3,7 @@ package com.cable.library.teleport;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.ITeleporter;
 
@@ -20,11 +21,19 @@ public class TeleportUtils {
         teleport(player, dimension, pos, player.rotationYaw, player.rotationPitch);
     }
 
+    public static void teleport(EntityPlayerMP player, int dimension, Vec3d pos) {
+        teleport(player, dimension, pos, player.rotationYaw, player.rotationPitch);
+    }
+
     public static void teleport(EntityPlayerMP player, int dimension, BlockPos pos, float yaw, float pitch) {
+        teleport(player, dimension, new Vec3d(pos.getX(), pos.getY(), pos.getZ()), yaw, pitch);
+    }
+
+    public static void teleport(EntityPlayerMP player, int dimension, Vec3d pos, float yaw, float pitch) {
         int from = player.getServerWorld().provider.getDimension();
 
         if (dimension == from) {
-            player.moveToBlockPosAndAngles(pos, yaw, pitch);
+            player.setLocationAndAngles(pos.x, pos.y, pos.z, yaw, pitch);
         } else {
             player.changeDimension(dimension, new SimpleTeleporter(pos, yaw, pitch));
         }
@@ -32,11 +41,17 @@ public class TeleportUtils {
 
     private static class SimpleTeleporter implements ITeleporter {
 
-        private BlockPos pos;
+        private Vec3d pos;
         private float yaw;
         private float pitch;
 
         private SimpleTeleporter(BlockPos pos, float yaw, float pitch) {
+            this.pos = new Vec3d(pos.getX(), pos.getY(), pos.getZ());
+            this.yaw = yaw;
+            this.pitch = pitch;
+        }
+
+        private SimpleTeleporter(Vec3d pos, float yaw, float pitch) {
             this.pos = pos;
             this.yaw = yaw;
             this.pitch = pitch;
@@ -44,7 +59,7 @@ public class TeleportUtils {
 
         @Override
         public void placeEntity(World world, Entity entity, float yaw) {
-            entity.moveToBlockPosAndAngles(this.pos, this.yaw, this.pitch);
+            entity.setLocationAndAngles(this.pos.x, this.pos.y, this.pos.z, this.yaw, this.pitch);
         }
     }
 }
